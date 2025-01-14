27ebb7ffc787aa0a1cab2520118349c78670dc41
Leandrocar13 <lecardonar@unal.edu.co>
2025-01-14 08:00:04 -0500

    Add files via upload

---
+++ EjercicioPropuesto7/src/ejerciciopropuesto7/EjercicioPropuesto7.java
@@ -0,0 +1,20 @@
+ /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejerciciopropuesto7;
+
+/**
+ *
+ * @author Leandro
+ */
+public class EjercicioPropuesto7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    VentanaPrincipal ventana= new VentanaPrincipal();
+    ventana.setVisible(true);
+    }
+ }
