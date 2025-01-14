0ffcb10bf2cc0967e35ad0e98d4770b625502e54
Leandrocar13 <lecardonar@unal.edu.co>
2025-01-14 08:00:47 -0500

    Add files via upload

---
+++ Actividad 3/EjercicioPropuesto7/src/ejerciciopropuesto7/Identificar.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejerciciopropuesto7;
+
+/**
+ *
+ * @author Leandro
+ */
+public class Identificar {
+    
+    double a;
+    double b;
+    
+         
+    public static String identificar(double a, double b){ 
+    if (a>b){
+        return "A es mayor que B ";
+        
+    }
+    else if (a==b){
+        return " A es igual a B ";
+        
+        
+                
+    }
+    
+    else {
+        return "A es menor que B";
+    }
+  }
+    
+    
+     public static String LimpiarCampo (){
+        return "";
+      }
+}
+    
+
